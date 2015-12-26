aad7322b4b6875cc4ac369315e952a386b85ac53
Sean Francis N. Ballais <sfballais123@gmail.com>
2015-12-26 18:15:52 +0800

    Added additional code.

--- outline/info.py
+++ outline/info.py
@@ -4,7 +4,7 @@ import version
 
 def info():
     print("Outline v{0}\n".format(version.__version__))
-    print("Copyright (c) Sean Francis N. Ballas. All rights reserved 2015.\n")
+    print("Copyright (c) Sean Francis N. Ballais. All rights reserved 2015.\n")
 
 if __name__ == "__main__":
     info()
--- outline/outline.py
+++ outline/outline.py
@@ -3,14 +3,20 @@
 import sys
 import getopt
 
+import help
+import info
+
 def main(argv):
     # Handle the command line arguments
     scripts_location = ""
     config_file = ""
     project_name = ""
 
-    
-
+    if "-h" or "--help" in argv or not argv:
+        help.help()
+    else if argv[0] == "-V" or argv[0] == "--version":
+        info.info()
+    sys.exit(0)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
